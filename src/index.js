f4e3a08905ebb2337fc98ebd1fe442843da62fac
DatNguyen12c <datnt@cozwork.com>
2021-03-05 15:35:25 +0700

    use boostrap

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const port = 3000;
+const path = require('path');
+const handlebars  = require('express-handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(morgan('combined'))
+
+// Template engine
+app.engine('hbs', handlebars({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
